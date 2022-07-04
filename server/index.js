4236e6cedeba72b6bb0c12c23881f5323dc2c00c
Fawez11 <fawezferjaoui11@gmail.com>
2022-07-04 17:46:32 +0200

    connect mongoose

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-// work here
+const express = require("express");
+const db = require("./db/schema");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.static("./client/build"));
+app.use(express.urlencoded({ extended: true }));
+const mongoose = require("mongoose");
+
+
+//mongoose connection  
+mongoose
+  .connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Hey u are connected To db...");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to DB");
+  });
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
