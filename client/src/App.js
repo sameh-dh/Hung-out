57a727a1747172e46f2d4b43b23cb983d8ac134f
Badre-Edin <dridibadr@gmail.com>
2022-07-05 18:36:51 +0200

    change view done

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import AddTrips from './AddTrips';
 import './App.css'
 import data from './data';
@@ -5,10 +6,31 @@ import Home from './homePage';
 
 
 function App() {
+
+  const [showHome, setShowHome] = useState(true);
+  const [showAddTrips, setShowAddTrips] = useState(false);
+
+  const onHomeClick = function () {
+    setShowHome(showHome => true)
+    setShowAddTrips(showAddTrips => false)
+
+  }
+  const onAddTripsClick = function () {
+    setShowHome(showHome => false)
+    setShowAddTrips(showAddTrips => true)
+
+  }
+
   return (
     <div className="App">
-    <Home data={data} />
-    <AddTrips />
+      <div className="header">
+        <nav className="navbar">
+          <div className="navBarLink" onClick={onHomeClick}>Home</div>
+          <div className="navBarLink" onClick={onAddTripsClick}>Add Trips</div>
+        </nav>
+      </div>
+      {showHome && <Home data={data} />}
+      {showAddTrips && <AddTrips />}
     </div>
   );
 }
