b73d073863550d46957db55115df8bcb640bdabb
sameh-dh <samehdh44@gmail.com>
2022-07-06 14:09:38 +0100

    sign up css

--- client/src/signUp.js
+++ client/src/signUp.js
@@ -3,11 +3,13 @@ import  { useState } from 'react';
     return (
         <div className="register">
          <h1>Sign Up</h1>
-         <input type="text" placeholder='username'></input>
+         <input type="text" placeholder='username' ></input>
          <input type="password" placeholder='password'></input>
+         <button className='button'>Sign Up</button>
          <h1>Sign in</h1>
          <input type="text" placeholder='username'></input>
          <input type="password" placeholder='password'></input>
+         <button className='button'>Sign in</button>
                      
         </div>
     )
