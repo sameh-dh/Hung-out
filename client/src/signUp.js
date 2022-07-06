2510e1bb793488831193279b73eb1fc8ba99aaa5
sameh-dh <samehdh44@gmail.com>
2022-07-06 14:07:55 +0100

    sign up page

---
+++ client/src/signUp.js
@@ -0,0 +1,16 @@
+import  { useState } from 'react';
+ function Register  (){
+    return (
+        <div className="register">
+         <h1>Sign Up</h1>
+         <input type="text" placeholder='username'></input>
+         <input type="password" placeholder='password'></input>
+         <h1>Sign in</h1>
+         <input type="text" placeholder='username'></input>
+         <input type="password" placeholder='password'></input>
+                     
+        </div>
+    )
+
+ }
+ export default Register;
