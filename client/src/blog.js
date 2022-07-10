dc6c0a345dbf1bf837094c19265421fd756e0af7
sameh-dh <samehdh44@gmail.com>
2022-07-10 13:04:40 +0100

    more updates in the bloging page

--- client/src/blog.js
+++ client/src/blog.js
@@ -1,22 +1,91 @@
 import React, { useState } from 'react';
 import blogData from './dataBlog';
 
-function Blog (){
-return (
-    <div className="blog-container" >
-      
-      { blogData.map((item, index) => {
-        return <div className="blog-item" key={index}>
-            <h1>{item.user}</h1>
-            <h2>{item.blog}</h2>
-         <img src={item.picture} alt={item.blog} className="image" />
-        
-        </div>;
-       }
-    )}
-    </div>
+function Blog() {
+    //like button
+    const [liked, setLiked] = useState(false)
+    //check if there is picture 2 
+    const picture = (item) => {
+        if (item.picture2 === undefined) {
+            return (
+                <div>
+                    <img src={item.picture} alt={item.blog} className="image-blog" />
+                </div>
+            )
+        } else if (!item.picture2 === undefined && item.picture3 === undefined) {
+            return (
+                <div>
+                    <img src={item.picture} alt={item.blog} className="image-blog" />
+                    <img src={item.picture2} alt={item.blog} className="image-blog" />
+                </div>
+            )
+        } else if (!item.picture3 === undefined && item.picture4 === undefined) {
+            return (
+                <div>
+                    <img src={item.picture} alt={item.blog} className="image-blog" />
+                    <img src={item.picture2} alt={item.blog} className="image-blog" />
+                    <img src={item.picture3} alt={item.blog} className="image-blog" />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <img src={item.picture} alt={item.blog} className="image-blog" />
+                    <img src={item.picture2} alt={item.blog} className="image-blog" />
+                    <img src={item.picture3} alt={item.blog} className="image-blog" />
+                    <img src={item.picture4} alt={item.blog} className="image-blog" />
+                </div>
+            )
+        }
+    }
+    // like button function
+ const like = (item,key) => {
+        if (liked === false) {
+            return (
+                <div>
+                    <button class="btn-secondary like-review" key={key} onClick={() => setLiked(true)}>
+                        <i class="fa fa-heart" ></i> {item.like}</button>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <button class="btn-secondary like-review" key={key} onClick={() => setLiked(false)}>
+                        <i class="fa fa-heart" ></i> {item.liked}</button>
+                </div>
+            )
+        }
+    }
+    return (
+        <div className="blog-container" >
 
-)
+            {blogData.map((item, index) => {
+                return <div className="blog-item" key={index}>
+                    <h1>{item.user}</h1>
+                    <h2>{item.blog}</h2>
+                    {picture(item)}
+                    {/* like button */}
+                    {like(item,index)}
+                </div>;
+            }
+            )}
+            <div className="blog-add">
+
+                {/* add blog */}
+                <h1>Add Blog</h1>
+                <input type="text" placeholder="your user name" />  <br />
+                <input type="text" placeholder="add your blog here" className="blog-input" />  <br />
+                <input type="text" placeholder="image1" />  <br />
+                <input type="text" placeholder="image2" />  <br />
+                <input type="text" placeholder="image3" />  <br />
+                <input type="text" placeholder="image4" />  <br />
+                <button className="button">Add Blog</button>
+
+            </div>
+
+        </div>
+
+    )
 }
 
 export default Blog;
--- client/src/dataBlog.js
+++ client/src/dataBlog.js
@@ -2,18 +2,27 @@ const blogData=[
     {
         "user":"sameh",
         "blog":"i went to this trip last weekend and it was amazing",
-        "picture":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZO401aueZh212ZfCwcP8CKgB2AtC92o-yfA&usqp=CAU"
+        "picture":"https://i.ytimg.com/vi/tntJWPhwCzQ/maxresdefault.jpg",
+        "like":"Like",
+         "liked":"You liked this",
+
     },
     {
         "user":"fawez",
         "blog":"i went to this restaurent last night and the food there was something special",
         "picture":"https://cdn.sortiraparis.com/images/80/94708/664368-holy-le-resto-cache-passage-verdeau.jpg",
-        "picture2":"https://thecliff.tn/wp-content/uploads/2021/06/3-TRP_7045-1.jpg"
+        "picture2":"https://thecliff.tn/wp-content/uploads/2021/06/3-TRP_7045-1.jpg",
+         "picture3":"https://www.voyage-tunisie.info/wp-content/uploads/2019/10/The-Cliff.jpg",
+         "picture4":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3WPc_XosH7X3cELJcM7qKcSJFQi0jYbpyyw&usqp=CAU",
+         "like":"Like",
+         "liked":"You liked this",
     },
     {
         "user":"Bader",
         "blog":"i went to this cofe shop in paris with my friends and they have a special coffee to taste ",
-        "picture":"https://cdn.sortiraparis.com/images/80/1467/717737-le-top-des-coffee-shops-a-paris.jpg"
+        "picture":"https://cdn.sortiraparis.com/images/80/1467/717737-le-top-des-coffee-shops-a-paris.jpg",
+        "like":"Like",
+        "liked":"You liked this",
     }
 
 ]
